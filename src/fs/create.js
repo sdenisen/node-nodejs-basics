08d12271d6eaa027fc4c85cc7a7fee71e1da258f
Sergey Denisenko <sdenisenko@five9.com>
2024-10-07 23:18:04 +0100

    feat: implement method create file.

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,37 @@
+// import fs from 'node:fs/promises';
+const fs = requre('fs').promises;
+
 const create = async () => {
-    // Write your code here 
+    // Write your code here
+    // create.js - implement function that creates new file fresh.txt
+    // with content I am fresh and young inside of the files folder
+    // (if file already exists Error with message FS operation failed must be thrown)
+
+    const FILE_PATH = 'src/fs/files/fresh.txt';
+
+    const isFileExist = async (path) => {
+        fs.stat(path).then(() => {
+            throw new Error ("FS operation failed");
+        }).catch(error => {
+            return false;
+        })
+    }
+
+    const write = async (path) => {
+        fs.writeFile(path, 'I am fresh and young').then(() => {
+            console.log("File create")
+        }).catch(error=>{
+            console.error(e.message);
+        });
+    }
+
+    try {
+        if (await isFileExist(FILE_PATH) === false) {
+            write(FILE_PATH);
+        }
+    }catch (e) {
+        console.log(e.message);
+    }
 };
 
 await create();
