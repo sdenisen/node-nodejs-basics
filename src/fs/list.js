edf36e910c210f3dbc98a04b2439223ef262afd4
Sergey Denisenko <sdenisenko@five9.com>
2024-10-08 01:06:21 +0100

    implement: list and read modules; passed.

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import fs from "node:fs/promises"
 const list = async () => {
-    // Write your code here 
+    // Write your code here
+    // list.js - implement function that prints all array of filenames from files folder into
+    // console (if files folder doesn't exists Error with message FS operation failed must be thrown)
+
+    const folder_of_files = "src/fs/files"
+
+    fs.readdir(folder_of_files).then(files => {
+        files.forEach( f => {console.log(f)});
+    }).catch(() => {
+        throw Error("FS operation failed");
+    })
+
 };
 
 await list();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import fs from "node:fs/promises"
+
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+    // read.js - implement function that prints content of the fileToRead.txt
+    // into console (if there's no file fileToRead.txt Error with message FS operation failed must be thrown)
+
+    const file_to_read = "src/fs/files/fileToRead.txt"
+    fs.readFile(file_to_read, { encoding: 'utf8' }).then(content => {
+        console.log(content);
+    }).catch(() => {
+        throw Error("FS operation failed");
+    })
+
 };
 
 await read();
