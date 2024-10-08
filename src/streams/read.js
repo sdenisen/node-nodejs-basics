5623c46a1b1fe7c41f012fff88e70a0dad3de845
Sergey Denisenko <sdenisenko@five9.com>
2024-10-08 02:09:40 +0100

    implement: another modules like; compress/decompress, and read/write/transform; passed.

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+
+import process from "node:process"
+import fs from "node:fs"
+
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+    // read.js - implement function that reads file fileToRead.txt content using Readable Stream and prints it's content into
+
+    const file_path = 'src/streams/files/fileToRead.txt';
+
+    const stream = fs.createReadStream(file_path, { encoding: 'utf8' });
+    stream.on('error', (error) => {
+        console.error(`Ошибка при чтении файла: ${error.message}`);
+    });
+    stream.pipe(process.stdout);
 };
 
 await read();
