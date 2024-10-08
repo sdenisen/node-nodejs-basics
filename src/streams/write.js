f7083693c1598714327f078c8cf814d3551181d0
Sergey Denisenko <sdenisenko@five9.com>
2024-10-08 02:09:40 +0100

    implement: another modules like; compress/decompress, and read/write/transform; passed.

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import fs from "node:fs"
+
 const write = async () => {
-    // Write your code here 
+    // Write your code here
+    // write.js - implement function that writes process.stdin data into file fileToWrite.txt content using Writable Stream
+
+    const file_path = "src/streams/files/fileToWrite.txt";
+    fs.access(file_path, fs.constants.F_OK, (err)=>{
+        const write_stream = fs.createWriteStream(file_path);
+
+        process.stdin.pipe(write_stream);
+
+        process.on('SIGINT', () => {
+            console.log('\n stop signal received!');
+            writeStream.end();
+            process.exit();
+        });
+
+    })
 };
 
 await write();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import fs from 'node:fs';
+import zlib from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+
 const decompress = async () => {
-    // Write your code here 
+    // Write your code here
+    // decompress.js - implement function that decompresses archive.gz
+    // back to the fileToCompress.txt with same content as before compression using zlib and Streams API
+
+    const archive_path = 'src/zip/files/archive.gz';
+    const output_path = 'src/zip/files/fileAfterDecompression.txt';
+
+    const sourceStream = fs.createReadStream(archive_path);
+    const gunzipStream = zlib.createGunzip();
+    const destinationStream = fs.createWriteStream(output_path);
+    await pipeline(sourceStream, gunzipStream, destinationStream);
 };
 
 await decompress();
