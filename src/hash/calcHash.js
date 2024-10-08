e6292f3236b0e73f15521710696bb5a43d2f3ef9
Sergey Denisenko <sdenisenko@five9.com>
2024-10-08 01:35:29 +0100

    implement: calcHash modules; passed.

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { promises as fs } from 'fs';
+import { createHash } from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    // calcHash.js - implement function that calculates SHA256 hash for file
+    // fileToCalculateHashFor.txt and logs it into console as
+
+    const file_path = "src/hash/files/fileToCalculateHashFor.txt";
+    fs.stat(file_path).then(() => {
+            fs.readFile(file_path).then(file_buffer => {
+                const hash = createHash('sha256').update(file_buffer).digest('hex');
+                console.log(hash);
+            });
+    }).catch(error => {
+        console.log("error during reading the file");
+    });
 };
 
 await calculateHash();
