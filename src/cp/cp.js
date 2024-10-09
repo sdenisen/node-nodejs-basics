06f4a8fc42dbe4f2f9c293c086ad6e136e9ccefb
Sergey Denisenko <sdenisenko@five9.com>
2024-10-09 09:19:47 +0100

    feat: implement modules "child process" and "work threads"; passed.

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import child_process from "node:child_process"
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+    // cp.js - implement function spawnChildProcess that receives array of arguments args and creates child process from file
+    const child_name = './src/cp/files/script.js'
+    const child = child_process.fork(child_name, args);
+    child.send(process.stdin)
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+// spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(  ["someArgument1", "someArgument2",] );
